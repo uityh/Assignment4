461091e5ada563a3c8260dd54237fefb98b00919
uityh.uityh <uityh.mtk2134@columbia.edu>
2016-07-27 09:34:00 -0400

    Matthew Kellerman Assignment4

--- unique.c
+++ unique.c
@@ -17,21 +17,23 @@ for(x=0;x<strlen(arr);x++){
 	//the x+1 sees if the index of the array is equal to the ones after it
 	//the for loops go through each value and see if the ones after it are equal to the number it is on
 	for(y=x+1;y<strlen(arr);y++){
-		if(arr[x]==arr[y]&& counter==0){
-			//the &&counter is need because if the user enters the same thing multiple times, the printf statement will also print multiple times
-			printf("Not unique\n");
+		if(arr[x]==arr[y]){
+			//sees if values are equal to each other
 			counter=1;
 			break;
-		}
+
 	}
 }
 
-
+}
 
 
 
 if(counter==0){
 	printf("All the characters are unique.\n");
+}
+else{
+	printf("Not unique\n");
 }
 
 
